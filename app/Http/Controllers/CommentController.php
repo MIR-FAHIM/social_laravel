8fe9e7d0d0510ca4b10933a0e30431cef97adf1a
MirFahim <mirfahim@aapbd.com>
2025-11-03 13:35:57 +0600

    new

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -33,7 +33,7 @@ class CommentController extends Controller
             'post_content_id' => 'required|exists:contents,id',  // Assuming the content table is 'contents'
             'comment' => 'required|string',
             'comment_type' => 'required|in:comment,debate',
-            'color' => 'nullable|in:green,blue',
+            'color' => 'nullable|in:green,red',
             'status' => 'required|in:approved,pending,rejected',
         ]);
 
