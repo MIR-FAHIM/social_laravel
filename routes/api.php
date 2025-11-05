a03eb0d178bb45e31b8e588d53a6dd3f0eb20ab3
MirFahim <mirfahim@aapbd.com>
2025-11-05 18:15:58 +0600

    nre

--- routes/api.php
+++ routes/api.php
@@ -168,6 +168,7 @@ Route::get('/user', function (Request $request) {
 
 // profile image
 Route::post('/upload-profile-images', [ProfileImageController::class, 'uploadProfileImages']);
+Route::post('/upload-single-profile-images', [UserController::class, 'uploadImage']);
 Route::get('/profile-images', [ProfileImageController::class, 'getProfileImages']);
 Route::post('/update-image-order', [ProfileImageController::class, 'updateImageOrder']);
 Route::delete('/delete-profile-image', [ProfileImageController::class, 'deleteProfileImage']);
